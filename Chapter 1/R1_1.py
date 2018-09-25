b8fb04d778fc715fda7f2c5f01ec5365591b1b6a
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2018-09-25 14:33:27 +0530

    Add files via upload

---
+++ Chapter 1/R1_1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 14:26:21 2018
+
+@author: Soyam
+
+Write a short Python function, is_multiple(n, m), that takes two integer values and returns True if n is a multiple of m, that is, n = m*i for some integer i, and False otherwise.
+"""
+def is_multiple(n,m):
+    return True if(n%m==0) else False
+
+
+#print(is_multiple(3,6))
